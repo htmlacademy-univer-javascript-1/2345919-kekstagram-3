ab54a659bd9aa5442293f342787462a37ef277c7
София Семенова <sofisem@yandex.ru>
2023-04-29 01:34:44 +0300

    new

--- js/draw.js
+++ js/draw.js
@@ -10,11 +10,11 @@ const similarObjects = createObjects();
 similarObjects.forEach((object) => {
 
   const objectElement = pictureTemplate.cloneNode(true);
-  var fullPhoto = document.querySelector('.picture__img');
+  const fullPhoto = document.querySelector('.picture__img');
   fullPhoto.textContent = object.URL;
   objectElement.querySelector('.picture__likes').textContent = object.likes;
   objectElement.querySelector('.picture__comments').textContent = object.comments;
-  similarListFragment.appendChild(objectElement);
+  fragment.appendChild(objectElement);
 });
 
 pictures.appendChild(fragment);
